9db9708099fb8162b730d2e0e097ec0a11c0332a
WandererLLC <WandererLLC@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-09 02:20:24 +0000

    Checking in 0.81 since I made a mistake in checking in the 0.8 version prematurely (I shouldn't have labeled the latest release as 0.8, will remember for next time)
    git-svn-id: https://plugins.svn.wordpress.org/autochimp/tags/0.8@273299 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 88-autochimp.php
+++ 88-autochimp.php
@@ -2,12 +2,15 @@
 /*
 Plugin Name: AutoChimp
 Plugin URI: http://www.wandererllc.com/company/plugins/autochimp/
-Description: Gives users the ability to update their MailChimp mailing lists when users subscribe, unsubscribe, and update their WordPress profiles.
+Description: Gives users the ability to create MailChimp mail campaigns from blog posts. Also allows updating MailChimp mailing lists when users subscribe, unsubscribe, and update their WordPress profiles.
 Author: Kyle Chapman
-Version: 0.5
+Version: 0.8
 */
 
-require_once 'inc/MCAPI.class.php';
+if ( !class_exists( 'MCAPI' ) )
+{
+	require_once 'inc/MCAPI.class.php';
+}
 
 define( "WP88_MC_APIKEY", "wp88_mc_apikey" );
 define( "WP88_MC_LISTS", "wp88_mc_selectedlists" );
@@ -15,6 +18,14 @@ define( "WP88_MC_ADD", "wp88_mc_add" );
 define( "WP88_MC_DELETE", "wp88_mc_delete" );
 define( "WP88_MC_UPDATE", "wp88_mc_update" );
 define( "WP88_MC_TEMPEMAIL", "wp88_mc_tempemail" );
+define( "WP88_MC_CAMPAIGN_FROM_POST", "wp88_mc_campaign_from_post" );
+define( "WP88_MC_CAMPAIGN_CATEGORY", "wp88_mc_campaign_category" );
+define( "WP88_MC_CREATE_CAMPAIGN_ONCE", "wp88_mc_create_campaign_once" );
+define( "WP88_MC_SEND_NOW", "wp88_mc_send_now" );
+define( "WP88_MC_LAST_ERROR", "wp88_mc_last_error" );
+define( "WP88_MC_CAMPAIGN_CREATED", "wp88_mc_campaign" );
+
+define( "AC_DEFAULT_CATEGORY", "Any category" );
 
 define( "MMU_ADD", 1 );
 define( "MMU_DELETE", 2 );
@@ -22,18 +33,50 @@ define( "MMU_UPDATE", 3 );
 
 define( "WP88_SEARCHABLE_PREFIX", "wp88_mc" );
 
-add_action('user_register','OnEightEightRegisterUser');
-add_action('delete_user','OnEightEightDeleteUser');
-add_action('show_user_profile','OnEightEightAboutToUpdateUser');
-add_action('profile_update','OnEightEightUpdateUser' );
-add_action('admin_menu', 'OnEightEightPluginMenu');
+//
+//	Actions to hook to allow AutoChimp to do it's work
+//
+//	See:  http://codex.wordpress.org/Plugin_API/Action_Reference
+//
+add_action('admin_menu', 'OnPluginMenu');				// Sets up the menu and admin page
+add_action('user_register','OnRegisterUser');			// Called when a user registers on the site
+add_action('delete_user','OnDeleteUser');				//   "      "  "  "   unregisters "  "  "
+add_action('show_user_profile','OnAboutToUpdateUser');	// Little trickier for update...need to save email in order to track them down later
+add_action('profile_update','OnUpdateUser' );			// Uses the saved email to update the user.
+add_action('publish_post','OnPublishPost' );			// Called when an author publishes a post.
+add_action('xmlrpc_publish_post', 'OnPublishPost' );	// Same as above, but for XMLRPC
+add_action('publish_phone', 'OnPublishPost' );			// Same as above, but for email.  No idea why it's called "phone".
 
-function OnEightEightPluginMenu()
+//
+//	Filters to hook
+//
+add_filter( 'plugin_row_meta', 'add_plugin_links', 10, 2 ); // Expand the links on the plugins page
+
+//
+//	Function to create the menu and admin page handler
+//
+function OnPluginMenu()
 {
-	add_submenu_page('options-general.php', 'AutoChimp Options', 'AutoChimp Options', 'manage_options', basename(__FILE__), EightEightOptions );
+	add_submenu_page('options-general.php', 'AutoChimp Options', 'AutoChimp', 'manage_options', basename(__FILE__), AutoChimpOptions );
 }
 
-function EightEightOptions()
+// Inspired by NextGen Gallery by Alex Rabe
+function add_plugin_links($links, $file)
+{
+	if ( $file == plugin_basename(__FILE__) )
+	{
+		$links[] = '<a href="http://wordpress.org/extend/plugins/autochimp/">' . __('Overview', 'autochimp') . '</a>';
+		$links[] = '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=HPCPB3GY5LUQW&lc=US">' . __('Donate', 'autochimp') . '</a>';
+	}
+	return $links;
+}
+
+//
+//	This function is responsible for displaying the AutoChimp admin panel.  That
+//	happens at the very bottom, with the require statement.  The rest of the code
+//	is for saving the options.
+//
+function AutoChimpOptions()
 {
 	// Stop the user if they don't have permission
 	if (!current_user_can('manage_options'))
@@ -53,11 +96,11 @@ function EightEightOptions()
 		update_option( WP88_MC_APIKEY, $newAPIKey );
 
 		// Tell the user
-		print "<div id=\"message\" class=\"updated fade\"><p>Saved API Key!</p></div>";
+		print '<div id="message" class="updated fade"><p>Saved API Key!</p></div>';
 	}
 
-	// Save off the requested mailing lists here
-	if ( isset( $_POST['save_mailing_lists'] ) )
+	// Save off the autochimp options here
+	if ( isset( $_POST['save_autochimp_options'] ) )
 	{
 		// Security check
 		check_admin_referer( 'mailchimpz-nonce' );
@@ -82,7 +125,7 @@ function EightEightOptions()
 		update_option( WP88_MC_LISTS, $selectionOption );
 
 		// Tell the user
-		print "<div id=\"message\" class=\"updated fade\"><p>Saved your mailing list selections!</p></div>";
+		print '<div id="message" class="updated fade"><p>Successfully saved your AutoChimp options!</p></div>';
 
 		// Step 2:  Save when the user wants to update the list
 
@@ -100,6 +143,26 @@ function EightEightOptions()
 			update_option( WP88_MC_UPDATE, "1" );
 		else
 			update_option( WP88_MC_UPDATE, "0" );
+
+		// Step 3:  Save the user's campaign-from-post choices
+
+		if ( isset( $_POST['on_campaign_from_post'] ) )
+			update_option( WP88_MC_CAMPAIGN_FROM_POST, "1" );
+		else
+			update_option( WP88_MC_CAMPAIGN_FROM_POST, "0" );
+
+		if ( isset( $_POST['on_send_now'] ) )
+			update_option( WP88_MC_SEND_NOW, "1" );
+		else
+			update_option( WP88_MC_SEND_NOW, "0" );
+
+		if ( isset( $_POST['on_create_once'] ) )
+			update_option( WP88_MC_CREATE_CAMPAIGN_ONCE, "1" );
+		else
+			update_option( WP88_MC_CREATE_CAMPAIGN_ONCE, "0" );
+
+		$category = $_POST['campaign_category'];
+		update_option( WP88_MC_CAMPAIGN_CATEGORY, $category );
 	}
 
 	// The file that will handle uploads is this one (see the "if" above)
@@ -107,6 +170,10 @@ function EightEightOptions()
 	require_once '88-autochimp-settings.php';
 }
 
+//
+//	Syncs a single user of this site with the options that the site owner has
+//	selected in the admin panel.
+//
 function ManageMailUser( $mode, $user_info )
 {
 	$apiKey = get_option( WP88_MC_APIKEY );
@@ -155,7 +222,7 @@ function ManageMailUser( $mode, $user_info )
 						{
 							// Get the old email - this feels a little dangerous...'cause users have to go
 							// through the profile panel.  If they don't and email is updated, data can
-							// get out of sync.  ADD TO README.
+							// get out of sync.  See the readme.txt for more.
 							$updateEmail = get_option( WP88_MC_TEMPEMAIL );
 
 							// Potential update to the email address (more likely than name!)
@@ -171,7 +238,97 @@ function ManageMailUser( $mode, $user_info )
 	}
 }
 
-function OnEightEightRegisterUser( $userID )
+//
+//	Given a post ID, creates a MailChimp campaign.  Returns STRING "-1" if the
+//	creation was skipped, "0" on failure, and a legit ID on success.  Except for
+//	"-1", each return point will write the latest result of the function to the
+//	DB which will be visible to the user in the admin page.
+//
+//	Pass the post ID and an instance of the MailChimp API class (for performance).
+//
+function CreateCampaignFromPost( $postID, $api )
+{
+	$myLists = $api->lists();
+
+	if ( null != $myLists )
+	{
+		$list_id = -1;
+
+		// See if the user has selected some lists
+		$selectedLists = get_option( WP88_MC_LISTS );
+
+		// Does the user only want to create campaigns once?
+		if ( "1" == get_option( WP88_MC_CREATE_CAMPAIGN_ONCE ) )
+		{
+			if ( "1" == get_post_meta( $postID, WP88_MC_CAMPAIGN_CREATED, true ) )
+				return "-1";	// Don't create the campaign again!
+		}
+
+		// Put all of the selected lists into an array to search later
+		$valuesArray = array();
+		$valuesArray = preg_split( "/[\s,]+/", $selectedLists );
+
+		// Get the info on this post
+		$post = get_post( $postID );
+
+		foreach ( $myLists as $list )
+		{
+			$list_id = $list['id'];
+
+			// See if this mailing list should have a campaign created for it
+			foreach( $valuesArray as $searchableID )
+			{
+				$pos = strpos( $searchableID, $list_id );
+				if ( false === $pos ){}
+				else
+				{
+					// Time to start creating the campaign...
+					// First, create the options array
+					$options = array();
+					$options['list_id']	= $list_id;
+					$options['subject']	= $post->post_title;
+					$options['from_email'] = $list['default_from_email'];
+					$options['to_email'] = "*|FNAME|*";
+					$options['from_name'] = $list['default_from_name'];
+					$options['tracking'] = array('opens' =>	true, 'html_clicks' => true, 'text_clicks' => false );
+					$options['authenticate'] = true;
+
+					$postContent = apply_filters( 'the_content', $post->post_content );
+					$postContent = str_replace( ']]>', ']]&gt;', $postContent );
+					$content = array();
+					$content['html'] = $postContent;
+					$content['text'] = strip_tags( $postContent );
+
+					// More info here:  http://www.mailchimp.com/api/1.2/campaigncreate.func.php
+					$result = $api->campaignCreate( "regular", $options, $content );
+					if ($api->errorCode)
+					{
+						// Set latest activity - displayed in the admin panel
+						update_option( WP88_MC_LAST_ERROR, "Problem with campaign with title '$post->post_title'.  Error Code: $api->errorCode, Message: $api->errorMessage" );
+						$result = "0";
+					}
+					else
+					{
+						// Set latest activity
+						update_option( WP88_MC_LAST_ERROR, "Your latest campaign created is titled '$post->post_title' with ID: $result" );
+
+						// Mark this post as having a campaign created from it.
+						add_post_meta( $postID, WP88_MC_CAMPAIGN_CREATED, "1" );
+					}
+
+					// Done
+					return $result;
+				}
+			}
+		}
+	}
+}
+
+//
+//	WordPress Action handlers
+//
+
+function OnRegisterUser( $userID )
 {
 	$user_info = get_userdata( $userID );
 	$onAddSubscriber = get_option( WP88_MC_ADD );
@@ -181,7 +338,7 @@ function OnEightEightRegisterUser( $userID )
 	}
 }
 
-function OnEightEightDeleteUser( $userID )
+function OnDeleteUser( $userID )
 {
 	$user_info = get_userdata( $userID );
 	$onDeleteSubscriber = get_option( WP88_MC_DELETE );
@@ -191,7 +348,7 @@ function OnEightEightDeleteUser( $userID )
 	}
 }
 
-function OnEightEightAboutToUpdateUser( $userID )
+function OnAboutToUpdateUser( $userID )
 {
 	$user_info = get_userdata( $userID );
 	$onUpdateSubscriber = get_option( WP88_MC_UPDATE );
@@ -202,7 +359,7 @@ function OnEightEightAboutToUpdateUser( $userID )
 	}
 }
 
-function OnEightEightUpdateUser( $userID )
+function OnUpdateUser( $userID )
 {
 	$user_info = get_userdata( $userID );
 	$onUpdateSubscriber = get_option( WP88_MC_UPDATE );
@@ -210,6 +367,50 @@ function OnEightEightUpdateUser( $userID )
 	{
 		ManageMailUser( MMU_UPDATE, $user_info );
 		update_option( WP88_MC_TEMPEMAIL, "" );
+	}
+}
+
+function OnPublishPost( $postID )
+{
+	// Does the user want to create campaigns from posts
+	$campaignFromPost = get_option( WP88_MC_CAMPAIGN_FROM_POST );
+	if ( "1" == $campaignFromPost )
+	{
+		// Get the info on this post
+		$post = get_post( $postID );
+		$categories = get_the_category( $postID );	// Potentially several categories
+
+		// What category does the user want to use to create campaigns?
+		$campaignCategory = get_option( WP88_MC_CAMPAIGN_CATEGORY );
+
+		// If it matches the user's category choice or is any category, then
+		// do the work.  This needs to be a loop because a post can belong to
+		// multiple categories.
+		foreach( $categories as $category )
+		{
+			if ( $category->name == $campaignCategory || AC_DEFAULT_CATEGORY == $campaignCategory )
+			{
+				// Create an instance of the MailChimp API
+				$apiKey = get_option( WP88_MC_APIKEY );
+				$api = new MCAPI( $apiKey );
+
+				// Do the work
+				$id = CreateCampaignFromPost( $postID, $api );
+
+				// Does the user want to send the campaigns right away?
+				$sendNow = get_option( WP88_MC_SEND_NOW );
+
+				// Send it, if necessary (if user wants it, and the $id is
+				// sufficiently long (just picking 2 for fun).
+				if ( "1" == $sendNow && ( strlen( $id ) > 2 ) )
+				{
+					$api->campaignSendNow( $id );
+				}
+
+				// As soon as the first match is found, break out.
+				break;
+			}
+		}
 	}
 }
 
